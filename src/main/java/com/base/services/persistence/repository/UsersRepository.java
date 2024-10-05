39d45f6905c1b938b6cb6af0b6b7bb1c2ef6e614
AbineshKrishnan <abineshr@piraiinfo.com>
2024-10-05 13:41:23 +0530

    repo changes

--- src/main/java/com/base/services/persistence/repository/UsersRepository.java
+++ src/main/java/com/base/services/persistence/repository/UsersRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface UsersRepository extends R2dbcRepository<UUID, Users> {
+public interface UsersRepository extends R2dbcRepository<Users, UUID> {
 }
